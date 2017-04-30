ccba370be5424fef1bea63eb42a0b7ba96844468
KK0001 <kkofkk01@gmail.com>
2017-04-30 18:07:17 +0900

    fix getTwiPics_TL.py

--- getTwiPics_TL.py
+++ getTwiPics_TL.py
@@ -4,12 +4,7 @@
 ###
 # タイムライン上の画像を保存する。
 # bashにTLを流して、画像を見つけたら保存。
-# 保存先としては、投稿者ユーザーのID(@~)のフォルダを『カレントディレクトリ/streamPics/userID』。
-#
-# 参考:
-# http://kivantium.hateblo.jp/entry/2015/01/03/000225
-# https://gist.github.com/kurozumi/d1228fc0761eb3dc128ad1b2abd8f818#file-twi-streaming-py
-# http://thetree.hatenadiary.jp/entry/2017/02/19/python_標準出力のフラッシュ_sys.stdout.flush
+# 保存先としては、投稿者ユーザーのID(@~)のフォルダを『カレントディレクトリ/picsOnStream/userID』。
 ###
 
 import sys
@@ -17,7 +12,9 @@ import tweepy
 import urllib
 import os
 import time
+from datetime import timedelta
 
+# oauthをget
 def get_oauth():
     CONSUMER_KEY=''
     CONSUMER_SECRET=''
@@ -31,16 +28,21 @@ def get_oauth():
 class Listener(tweepy.StreamListener):
     def on_status(self, status):
         print('------------------------------')
-        print(status.text)
+        print(status.text) #呟きを表示
+        status.created_at += timedelta(hours=9) #表示時間を日本時間に対応
+        # 投稿者情報を表示
         print(u"{name}({screen}) {created} via {src}\n".format(
                                                                name=status.author.name, screen=status.author.screen_name,
                                                                created=status.created_at, src=status.source))
 
-        if os.path.exists("./picsOnStream/" + status.author.screen_name) == False:
-            os.makedirs("./picsOnStream/" + status.author.screen_name)
-
+        # 画像を見つけた時に保存する
         # if hasattr(status, "extended_entities"):
         if "media" in status.extended_entities:
+
+            #ユーザーフォルダが無かったら作成
+            if os.path.exists("./picsOnStream/" + status.author.screen_name) == False:
+                os.makedirs("./picsOnStream/" + status.author.screen_name)
+
             for index,media in enumerate(status.extended_entities["media"]):
                 img_url = media["media_url_https"]
                 print(status.author.screen_name + " image " +  str(img_url) + " save to ./picsOnStream/" + status.author.screen_name)
@@ -68,5 +70,5 @@ if __name__ == '__main__':
         try:
             stream.userstream()
         except:#Twitterに弾かれた時の対策
-            time.sleep(10)
+            time.sleep(3)
             stream = tweepy.Stream(auth, Listener())
